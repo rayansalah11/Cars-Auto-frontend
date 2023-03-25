0f2d56f1f42b30d86627ebaa3745374b0ac101dc
RAYAN SALAH <rayan.salah11@gmail.com>
2023-03-25 12:11:06 +0300

    first commit

---
+++ car/src/components/common/header/Head.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> 00960000000</label>
+            <i className='fa fa-envelope'></i>
+            <label> autocars2023@gmail.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Need Help?</label>
+            <span>🏳️‍⚧️</span>
+            <label>EN</label>
+            <span>🏳️‍⚧️</span>
+            <label>Ar</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
