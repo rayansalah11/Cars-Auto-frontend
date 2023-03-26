135655b6df88b66b79a40220292180cb1edf9074
RAYAN SALAH <rayan.salah11@gmail.com>
2023-03-26 22:25:48 +0300

    commit

---
+++ car/src/components/common/header/Search.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const Search = () => {
+ 
+  return (
+    <>
+      <section className='search'>
+        <div className='container c_flex'>
+          <div className='logo width '>
+            {/* <img src={logo} alt='' /> */}
+          </div>
+
+          <div className='search-box f_flex'>
+            <i className='fa fa-search'></i>
+            <input type='text' placeholder='Search and hit enter...' />
+            <span>All Category</span>
+          </div>
+
+          <div className='icon f_flex width'>
+            <i className='fa fa-user icon-circle'></i>
+            <div className='cart'>
+              <Link to='/cart'>
+                <i className='fa fa-shopping-bag icon-circle'></i>
+                <span>0</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Search
