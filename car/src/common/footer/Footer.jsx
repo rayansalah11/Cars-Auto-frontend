db464ac44a77d34082f22c868e2c3091f70f15ad
RAYAN SALAH <rayan.salah11@gmail.com>
2023-03-28 17:01:05 +0300

    commit

--- car/src/common/footer/Footer.jsx
+++ car/src/common/footer/Footer.jsx
@@ -9,16 +9,7 @@ const Footer = () => {
           <div className='box'>
             <h1>AutoCars</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat et lectus vel ut sollicitudin elit at amet.</p>
-            {/* <div className='icon d_flex'>
-              <div className='img d_flex'>
-                <i class='fa-brands fa-google-play'></i>
-                <span>Google Play</span>
-              </div>
-              <div className='img d_flex'>
-                <i class='fa-brands fa-app-store-ios'></i>
-                <span>App Store</span>
-              </div>
-            </div> */}
+           
           </div>
 
           <div className='box'>
