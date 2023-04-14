43ab3a1dd4a35eac466dde6fb50c8d80a7528c9f
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-14 23:54:22 +0300

    commit

--- src/component/Authentication/LoginSignup.jsx
+++ src/component/Authentication/LoginSignup.jsx
@@ -1,4 +1,12 @@
 import { React, useEffect, useRef, useState } from "react";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import LockOpenIcon from "@material-ui/icons/LockOpen";
-i
+import FaceIcon from "@material-ui/icons/Face";
+import { Link } from "react-router-dom";
+import "./LoginSignup.css";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../../more/Loader";
+import { clearErrors, login, register } from "../../actions/userAction";
+import MetaData from "../../more/Metadata";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
