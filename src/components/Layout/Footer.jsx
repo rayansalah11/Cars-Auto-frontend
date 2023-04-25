92c472a2a3a8a2481dcadda53b30f8936c98e405
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-26 00:35:57 +0300

    convert redux to toolkit

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -15,11 +15,11 @@ import {
 const Footer = () => {
   return (
     <div className="bg-[#000] text-white">
-      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#342ac8] py-7">
+      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#4d4e52] py-7">
         <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
           <span className="text-[#56d879]">Subscribe</span> us for get news{" "}
           <br />
-          events and offers
+          offers
         </h1>
         <div>
           <input
