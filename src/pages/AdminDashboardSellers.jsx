d82d5cc81260502968ab0444bcda5dcc077a3848
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:20:27 +0300

    commit

--- src/pages/AdminDashboardSellers.jsx
+++ src/pages/AdminDashboardSellers.jsx
@@ -8,5 +8,15 @@ const AdminDashboardSellers = () => {
     <div>
     <AdminHeader />
     <div className="w-full flex">
-   
+      <div className="flex items-start justify-between w-full">
+        <div className="w-[80px] 800px:w-[330px]">
+          <AdminSideBar active={3} />
+        </div>
+        <AllSellers />
+      </div>
+    </div>
+  </div>
+  )
+}
+
 export default AdminDashboardSellers
