37944b89fa8e60fee56a53029dd60a9498699eec
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:21:43 +0300

    commit

--- src/pages/AdminDashboardUsers.jsx
+++ src/pages/AdminDashboardUsers.jsx
@@ -10,5 +10,13 @@ const AdminDashboardUsers = () => {
     <div className="w-full flex">
       <div className="flex items-start justify-between w-full">
         <div className="w-[80px] 800px:w-[330px]">
-       
+          <AdminSideBar active={4} />
+        </div>
+        <AllUsers />
+      </div>
+    </div>
+  </div>
+  )
+}
+
 export default AdminDashboardUsers
