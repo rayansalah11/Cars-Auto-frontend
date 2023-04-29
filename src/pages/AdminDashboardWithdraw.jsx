2de5c3db307d6b7965b52ec1875a04c078f2a7e6
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:22:42 +0300

    commit

--- src/pages/AdminDashboardWithdraw.jsx
+++ src/pages/AdminDashboardWithdraw.jsx
@@ -3,6 +3,9 @@ import AdminHeader from '../components/Layout/AdminHeader'
 import AdminSideBar from '../components/Admin/Layout/AdminSideBar'
 import AllWithdraw from "../components/Admin/AllWithdraw";
 
-
+const AdminDashboardWithdraw = () => {
+  return (
+    <div>
+  
 
 export default AdminDashboardWithdraw
