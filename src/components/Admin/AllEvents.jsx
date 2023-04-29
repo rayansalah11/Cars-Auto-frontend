7b640cd2d6717a665ed1dea607bd6d4fa3e5d47f
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:03:08 +0300

    commit

--- src/components/Admin/AllEvents.jsx
+++ src/components/Admin/AllEvents.jsx
@@ -21,5 +21,11 @@ const AllEvents = () => {
       headerName: "Name",
       minWidth: 180,
       flex: 1.4,
-   
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      minWidth: 100,
+      flex: 0.6,
+  
 export default AllEvents;
