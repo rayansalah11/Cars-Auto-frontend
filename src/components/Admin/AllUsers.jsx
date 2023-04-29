b45aa506d49389197c67ee7a69174f37aff53895
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 22:50:19 +0300

    Alluser

--- src/components/Admin/AllUsers.jsx
+++ src/components/Admin/AllUsers.jsx
@@ -6,6 +6,10 @@ import { DataGrid } from "@material-ui/data-grid";
 import { AiOutlineDelete } from "react-icons/ai";
 import { Button } from "@material-ui/core";
 import styles from "../../styles/styles";
+import { RxCross1 } from "react-icons/rx";
+import axios from "axios";
+import { server } from "../../server";
+import { toast } from "react-toastify";
 
 
 export default AllUsers;
