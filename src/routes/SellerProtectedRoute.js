46683df9e03afc8486d7ed5280cf2f6e39c27cb2
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:32:42 +0300

    commit

--- src/routes/SellerProtectedRoute.js
+++ src/routes/SellerProtectedRoute.js
@@ -5,7 +5,13 @@ import Loader from "../components/Layout/Loader";
 const SellerProtectedRoute = ({ children }) => {
   const { isLoading, isSeller } = useSelector((state) => state.seller);
   if (isLoading === true) {
-  
+    return <Loader />;
+  } else {
+    if (!isSeller) {
+      return <Navigate to={`/shop-login`} replace />;
+    }
+    return children;
+  }
 };
 
 export default SellerProtectedRoute;
