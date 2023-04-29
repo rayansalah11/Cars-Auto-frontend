5018ace39ee0d61dcd325c7d478ce8a8cbac57ad
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:30:10 +0300

    commit

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,8 @@
 import LoginPage from "../pages/Login";
 import SignupPage from "../pages/SignupPage";
+import ActivationPage from "../pages/ActivationPage";
+import HomePage from "../pages/HomePage";
+import ProductsPage from "../pages/ProductsPage";
 
 export { 
   
