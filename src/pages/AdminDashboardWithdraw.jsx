b235849006fd1b240a5a76a487581915c02c82a6
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:23:00 +0300

    commit

--- src/pages/AdminDashboardWithdraw.jsx
+++ src/pages/AdminDashboardWithdraw.jsx
@@ -10,6 +10,13 @@ const AdminDashboardWithdraw = () => {
     <div className="w-full flex">
       <div className="flex items-start justify-between w-full">
         <div className="w-[80px] 800px:w-[330px]">
-  
+          <AdminSideBar active={7} />
+        </div>
+         <AllWithdraw />
+      </div>
+    </div>
+  </div>
+  )
+}
 
 export default AdminDashboardWithdraw
