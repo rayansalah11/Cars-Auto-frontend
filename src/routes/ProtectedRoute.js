a84b26b9fe4ef3114965b66ff5716abee3e23d7c
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:30:01 +0300

    commit

--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -2,6 +2,13 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const { loading, is
+  const { loading, isAuthenticated } = useSelector((state) => state.user);
+  if (loading === false) {
+    if (!isAuthenticated) {
+      return <Navigate to="/login" replace />;
+    }
+    return children;
+  }
+};
 
 export default ProtectedRoute;
