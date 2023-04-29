99e2e3f362cce46f7d031ef8288c5f0a9fd81e7d
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:29:05 +0300

    commit

--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -1,5 +1,7 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
+const ProtectedRoute = ({ children }) => {
+  const { loading, is
 
 export default ProtectedRoute;
