8ce795d22e74efbb723e5d592ec229ba0d856e87
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:20:01 +0300

    commit

--- src/pages/AdminDashboardSellers.jsx
+++ src/pages/AdminDashboardSellers.jsx
@@ -3,5 +3,7 @@ import AdminHeader from '../components/Layout/AdminHeader'
 import AdminSideBar from '../components/Admin/Layout/AdminSideBar'
 import AllSellers from "../components/Admin/AllSellers";
 
-
+const AdminDashboardSellers = () => {
+  return (
+  
 export default AdminDashboardSellers
