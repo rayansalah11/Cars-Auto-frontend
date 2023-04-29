04436bbdcd527158355ec40731808b64adbd9f5b
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:22:51 +0300

    commit

--- src/pages/AdminDashboardWithdraw.jsx
+++ src/pages/AdminDashboardWithdraw.jsx
@@ -6,6 +6,10 @@ import AllWithdraw from "../components/Admin/AllWithdraw";
 const AdminDashboardWithdraw = () => {
   return (
     <div>
+    <AdminHeader />
+    <div className="w-full flex">
+      <div className="flex items-start justify-between w-full">
+        <div className="w-[80px] 800px:w-[330px]">
   
 
 export default AdminDashboardWithdraw
