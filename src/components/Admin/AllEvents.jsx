beccdf717b2f0b0718f176442af8e791fdfd3e9b
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:02:19 +0300

    commit

--- src/components/Admin/AllEvents.jsx
+++ src/components/Admin/AllEvents.jsx
@@ -2,6 +2,7 @@ import { Button } from "@material-ui/core";
 import { DataGrid } from "@material-ui/data-grid";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import {  AiOutlineEye } from "react-icons/ai";
 
 
 export default AllEvents;
