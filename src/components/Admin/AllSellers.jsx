487113e7e9892fd52ece4e714ab50ba18776fdb6
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 22:45:39 +0300

    All sellers

---
+++ src/components/Admin/AllSellers.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { DataGrid } from "@material-ui/data-grid";
+import { AiOutlineDelete, AiOutlineEye } from "react-icons/ai";
+import { Button } from "@material-ui/core";
+import styles from "../../styles/styles";
+import { RxCross1 } from "react-icons/rx";
+import axios from "axios";
+import { server } from "../../server";
+import { toast } from "react-toastify";
+import { getAllSellers } from "../../redux/actions/sellers";
+import { Link } from "react-router-dom";
+
+const AllSellers = () => {
+  const dispatch = useDispatch();
+  const { sellers } = useSelector((state) => state.seller);
+  const [open, setOpen] = useState(false);
+  const [userId, setUserId] = useState("");
+
+ 
+export default AllSellers;
