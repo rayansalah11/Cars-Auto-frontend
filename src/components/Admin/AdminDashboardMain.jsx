0ff232825a4833bf73aa599d54e9adfce8afeabc
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 22:37:17 +0300

    AdminDashboardMain

---
+++ src/components/Admin/AdminDashboardMain.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import styles from "../../styles/styles";
+import { AiOutlineArrowRight, AiOutlineMoneyCollect } from "react-icons/ai";
+import { MdBorderClear } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { DataGrid } from "@material-ui/data-grid";
+import { Button } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllOrdersOfAdmin } from "../../redux/actions/order";
+import Loader from "../Layout/Loader";
+import { getAllSellers } from "../../redux/actions/sellers";
+
+
+export default AdminDashboardMain;
