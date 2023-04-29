1799bf256d61f4faa77c10506a341e1199aa0c43
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 22:50:03 +0300

    Alluser

--- src/components/Admin/AllUsers.jsx
+++ src/components/Admin/AllUsers.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getAllUsers } from "../../redux/actions/user";
 
 
 export default AllUsers;
