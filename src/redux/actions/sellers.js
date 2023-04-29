fba01daa71a80407c9019299f1577784be9fac09
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:26:52 +0300

    commit

--- src/redux/actions/sellers.js
+++ src/redux/actions/sellers.js
@@ -3,4 +3,11 @@ import { server } from "../../server";
 
 // get all sellers --- admin
 export const getAllSellers = () => async (dispatch) => {
- 
+  try {
+    dispatch({
+      type: "getAllSellersRequest",
+    });
+
+    const { data } = await axios.get(`${server}/shop/admin-all-sellers`, {
+      withCredentials: true,
+    });
