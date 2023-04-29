3e261bfc321354d31876c39b1e6afa6d19778679
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:19:18 +0300

    commit

--- src/pages/AdminDashboardProducts.jsx
+++ src/pages/AdminDashboardProducts.jsx
@@ -11,6 +11,12 @@ const AdminDashboardProducts = () => {
       <div className="flex items-start justify-between w-full">
         <div className="w-[80px] 800px:w-[330px]">
           <AdminSideBar active={5} />
-    
+        </div>
+        <AllProducts />
+      </div>
+    </div>
+  </div>
+  )
+}
 
 export default AdminDashboardProducts
