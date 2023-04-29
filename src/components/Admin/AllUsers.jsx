83fa22e3f64014d645b35d23d82249ec071ba860
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 22:50:11 +0300

    Alluser

--- src/components/Admin/AllUsers.jsx
+++ src/components/Admin/AllUsers.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getAllUsers } from "../../redux/actions/user";
+import { DataGrid } from "@material-ui/data-grid";
+import { AiOutlineDelete } from "react-icons/ai";
+import { Button } from "@material-ui/core";
+import styles from "../../styles/styles";
 
 
 export default AllUsers;
