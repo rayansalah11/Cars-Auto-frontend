59651900adb2bd8aa2b9c66e062584baf6db7470
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:21:26 +0300

    commit

--- src/pages/AdminDashboardUsers.jsx
+++ src/pages/AdminDashboardUsers.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AdminHeader from '../components/Layout/AdminHeader'
 import AdminSideBar from '../components/Admin/Layout/AdminSideBar'
+import AllUsers from "../components/Admin/AllUsers";
 
 
 export default AdminDashboardUsers
