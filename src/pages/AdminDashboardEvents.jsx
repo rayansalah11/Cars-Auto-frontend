c0d6a5767d29c5d477f091966b2e15b9b4fffd0e
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:11:51 +0300

    commit

--- src/pages/AdminDashboardEvents.jsx
+++ src/pages/AdminDashboardEvents.jsx
@@ -3,6 +3,8 @@ import AdminHeader from '../components/Layout/AdminHeader'
 import AdminSideBar from '../components/Admin/Layout/AdminSideBar'
 import AllEvents from '../components/Admin/AllEvents';
 
-
+const AdminDashboardEvents = () => {
+  return (
+ 
 
 export default AdminDashboardEvents
