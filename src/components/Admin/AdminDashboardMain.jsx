6c04b03f44d1fb0e5955398a6a078a15648cedcd
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 22:39:21 +0300

    AdminDashboardMain

--- src/components/Admin/AdminDashboardMain.jsx
+++ src/components/Admin/AdminDashboardMain.jsx
@@ -101,6 +101,14 @@ const AdminDashboardMain = () => {
             <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">$ {adminBalance}</h5>
           </div>
   
+          <div className="w-full mb-4 800px:w-[30%] min-h-[20vh] bg-white shadow rounded px-2 py-5">
+            <div className="flex items-center">
+              <MdBorderClear size={30} className="mr-2" fill="#00000085" />
+              <h3
+                className={`${styles.productTitle} !text-[18px] leading-5 !font-[400] text-[#00000085]`}
+              >
+                All Sellers
+        
         
 
 export default AdminDashboardMain;
