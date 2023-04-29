076f9e46f49020565a4ab0fc9001713145a13d67
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 22:57:28 +0300

    commit

--- src/components/Admin/AllWithdraw.jsx
+++ src/components/Admin/AllWithdraw.jsx
@@ -27,5 +27,14 @@ const AllWithdraw = () => {
       });
   }, []);
 
+  const columns = [
+    { field: "id", headerName: "Withdraw Id", minWidth: 150, flex: 0.7 },
+    {
+      field: "name",
+      headerName: "Shop Name",
+      minWidth: 180,
+      flex: 1.4,
+    },
+   
 
 export default AllWithdraw;
