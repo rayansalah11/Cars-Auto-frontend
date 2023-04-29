eb9a9af069c3c634998c2fc6f5643da574b570a2
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:32:23 +0300

    commit

---
+++ src/routes/SellerProtectedRoute.js
@@ -0,0 +1,7 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import Loader from "../components/Layout/Loader";
+
+
+
+export default SellerProtectedRoute;
