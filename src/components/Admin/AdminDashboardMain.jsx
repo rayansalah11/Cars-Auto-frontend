b7cbe171128f13c62fef3024168b5b413d6ebe75
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 22:39:55 +0300

    AdminDashboardMain

--- src/components/Admin/AdminDashboardMain.jsx
+++ src/components/Admin/AdminDashboardMain.jsx
@@ -129,7 +129,14 @@ const AdminDashboardMain = () => {
                 All Orders
               </h3>
             </div>
-        
+            <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">{adminOrders && adminOrders.length}</h5>
+            <Link to="/admin-orders">
+              <h5 className="pt-4 pl-2 text-[#077f9c]">View Orders</h5>
+            </Link>
+          </div>
+        </div>
+  
+        <br />
         
 
 export default AdminDashboardMain;
