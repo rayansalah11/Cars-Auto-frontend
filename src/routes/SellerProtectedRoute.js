edd089c31d9c121203cd3828229c031d0155c990
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:32:33 +0300

    commit

--- src/routes/SellerProtectedRoute.js
+++ src/routes/SellerProtectedRoute.js
@@ -2,6 +2,10 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import Loader from "../components/Layout/Loader";
 
-
+const SellerProtectedRoute = ({ children }) => {
+  const { isLoading, isSeller } = useSelector((state) => state.seller);
+  if (isLoading === true) {
+  
+};
 
 export default SellerProtectedRoute;
