127368d82f7eb3a552637bb37521bc2125153fad
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 22:39:29 +0300

    AdminDashboardMain

--- src/components/Admin/AdminDashboardMain.jsx
+++ src/components/Admin/AdminDashboardMain.jsx
@@ -108,7 +108,14 @@ const AdminDashboardMain = () => {
                 className={`${styles.productTitle} !text-[18px] leading-5 !font-[400] text-[#00000085]`}
               >
                 All Sellers
-        
+              </h3>
+            </div>
+            <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">{sellers && sellers.length}</h5>
+            <Link to="/admin-sellers">
+              <h5 className="pt-4 pl-2 text-[#077f9c]">View Sellers</h5>
+            </Link>
+          </div>
+  
         
 
 export default AdminDashboardMain;
