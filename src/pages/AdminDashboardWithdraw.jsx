000b9bc0b06f55def2fb57fe420a65571175ba59
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:22:26 +0300

    commit

--- src/pages/AdminDashboardWithdraw.jsx
+++ src/pages/AdminDashboardWithdraw.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AdminHeader from '../components/Layout/AdminHeader'
-
+import AdminSideBar from '../components/Admin/Layout/AdminSideBar'
+i
 
 
 export default AdminDashboardWithdraw
