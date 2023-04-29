b0176b5c79c3a608c19a71647e1122d5e95c0b80
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 22:47:37 +0300

    All sellers

--- src/components/Admin/AllSellers.jsx
+++ src/components/Admin/AllSellers.jsx
@@ -126,5 +126,12 @@ const AllSellers = () => {
           />
         </div>
         {open && (
-            
+          <div className="w-full fixed top-0 left-0 z-[999] bg-[#00000039] flex items-center justify-center h-screen">
+            <div className="w-[95%] 800px:w-[40%] min-h-[20vh] bg-white rounded shadow p-5">
+              <div className="w-full flex justify-end cursor-pointer">
+                <RxCross1 size={25} onClick={() => setOpen(false)} />
+              </div>
+              <h3 className="text-[25px] text-center py-5 font-Poppins text-[#000000cb]">
+                Are you sure you wanna delete this user?
+          
 export default AllSellers;
