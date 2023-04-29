0ec3b6925906d0f89afe899067ad7a0e7aea896f
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:18:32 +0300

    commit

--- src/pages/AdminDashboardProducts.jsx
+++ src/pages/AdminDashboardProducts.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
 import AdminHeader from '../components/Layout/AdminHeader'
+import AdminSideBar from '../components/Admin/Layout/AdminSideBar'
+import AllProducts from "../components/Admin/AllProducts";
 
 export default AdminDashboardProducts
