fbe8b8902d68aba711904fd39aa2cd176d236dde
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:33:36 +0300

    commit

--- src/routes/ShopRoutes.js
+++ src/routes/ShopRoutes.js
@@ -1,4 +1,8 @@
 import ShopDashboardPage from "../pages/Shop/ShopDashboardPage";
+import ShopCreateProduct from "../pages/Shop/ShopCreateProduct";
+import ShopAllProducts from "../pages/Shop/ShopAllProducts";
+import ShopCreateEvents from "../pages/Shop/ShopCreateEvents";
+import ShopAllEvents from "../pages/Shop/ShopAllEvents";
 
 export {
   
