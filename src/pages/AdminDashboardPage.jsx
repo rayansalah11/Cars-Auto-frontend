154caab0145967ce883c0ee4ddb371e1f1e8e8e6
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:12:40 +0300

    commit

--- src/pages/AdminDashboardPage.jsx
+++ src/pages/AdminDashboardPage.jsx
@@ -1,4 +1,6 @@
 import React from "react";
 import AdminHeader from "../components/Layout/AdminHeader";
+import AdminSideBar from "../components/Admin/Layout/AdminSideBar";
+import AdminDashboardMain from "../components/Admin/AdminDashboardMain";
 
 export default AdminDashboardPage;
