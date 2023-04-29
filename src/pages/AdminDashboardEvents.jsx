644446d01f639f71949d50488c92f8efcf617c0a
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 23:12:08 +0300

    commit

--- src/pages/AdminDashboardEvents.jsx
+++ src/pages/AdminDashboardEvents.jsx
@@ -10,6 +10,13 @@ const AdminDashboardEvents = () => {
     <div className="w-full flex">
       <div className="flex items-start justify-between w-full">
         <div className="w-[80px] 800px:w-[330px]">
-   
+          <AdminSideBar active={6} />
+        </div>
+        <AllEvents />
+      </div>
+    </div>
+  </div>
+  )
+}
 
 export default AdminDashboardEvents
