fbb915e1bb27d95e078a4463a36be64cf620f95e
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 22:52:33 +0300

    Alluser

--- src/components/Admin/AllUsers.jsx
+++ src/components/Admin/AllUsers.jsx
@@ -93,6 +93,15 @@ const AllUsers = () => {
         joinedAt: item.createdAt.slice(0, 10),
       });
     });
-     
+
+  return (
+    <div className="w-full flex justify-center pt-5">
+      <div className="w-[97%]">
+        <h3 className="text-[22px] font-Poppins pb-2">All Users</h3>
+        <div className="w-full min-h-[45vh] bg-white rounded">
+          <DataGrid
+            rows={row}
+            columns={columns}
+              
 
 export default AllUsers;
