cf3d0585e7eeca6df869f1f5efd28592012f58b5
RAYAN SALAH <rayan.salah11@gmail.com>
2023-06-04 21:51:42 +0300

    commit

--- src/pages/FAQPage.jsx
+++ src/pages/FAQPage.jsx
@@ -73,7 +73,7 @@ const Faq = () => {
               <p className="text-base text-gray-500">
                 If you're not satisfied with your purchase, we accept returns
                 within 30 days of delivery. To initiate a return, please email
-                us at support@myecommercestore.com with your order number and a
+                us at support@autocars.com with your order number and a
                 brief explanation of why you're returning the item.
               </p>
             </div>
@@ -171,8 +171,8 @@ const Faq = () => {
             <div className="mt-4">
               <p className="text-base text-gray-500">
                 You can contact our customer support team by emailing us at
-                support@myecommercestore.com, or by calling us at (555) 123-4567
-                between the hours of 9am and 5pm EST, Monday through Friday.
+                support@autocars.com, or by calling us at 00964 990....
+                between the hours of 9am and 5pm , Monday through Friday.
               </p>
             </div>
           )}
@@ -269,7 +269,7 @@ const Faq = () => {
           {activeTab === 6 && (
             <div className="mt-4">
               <p className="text-base text-gray-500">
-                Currently, we only offer shipping within the United States.
+                Currently, we only offer shipping within the iraq.
               </p>
             </div>
           )}
